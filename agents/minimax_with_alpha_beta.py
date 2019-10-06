a47cea33b5690fc1c17eb9d7620a61223aaf9e91
olga-gorun <olga@anyclip.com>
2019-10-06 14:10:32 +0300

    minimax with alpha-beta

---
+++ agents/minimax_with_alpha_beta.py
@@ -0,0 +1,50 @@
+from agent import Agent
+from game import Game
+import copy
+
+
+class MinimaxWithAlphaBeta(Agent):
+
+    def __init__(self, label, max_depth=10):
+        super().__init__(label)
+        self.max_depth = max_depth
+        self.min_score = -max_depth
+        self.max_score = max_depth
+
+    def move(self, game: Game):
+        result = self.__minimax(game, 0, self.min_score, self.max_score)
+        return result[0].state
+
+    def __minimax(self, game: Game, depth, alpha, beta, is_my_turn=True):
+        winner = game.evaluate()
+        if winner == self.label:
+            return (game, self.max_score-depth)
+        elif winner == 'draw':
+            return (game, 0)
+        elif winner is not None:
+            return (game, self.min_score+depth)
+        elif depth > self.max_depth:
+            return (game,  beta if is_my_turn else alpha)
+        else:
+            possible_moves = game.get_possible_next_states()
+            results = []
+            for state in possible_moves:
+                game_clone = copy.deepcopy(game)
+                game_clone.set_debug(False)
+                game_clone.move(state)
+                result = self.__minimax(game_clone, depth + 1, alpha, beta, not is_my_turn)
+                results.append((game_clone, result[1]))
+
+                if is_my_turn and alpha < result[1]:
+                    alpha = result[1]
+                elif not is_my_turn and result[1] < beta:
+                    beta = result[1]
+
+                if alpha >= beta:
+                    break
+
+            if is_my_turn:
+                final = max(results, key=lambda item: item[1])
+            else:
+                final = min(results, key=lambda item: item[1])
+            return final
--- play_test.py
+++ play_test.py
@@ -1,24 +1,26 @@
 from agents.random_agent import RandomAgent
 from agents.minimax_agent import MinimaxAgent
 from agents.depth_sensitive_minimax_agent import DepthSensitiveMinimaxAgent
+from agents.minimax_with_alpha_beta import MinimaxWithAlphaBeta
 from games.tictactoe import TicTacToe
 from tqdm import tqdm
 
 
 player_X = RandomAgent('X')
-player_O = MinimaxAgent('O')
+player_O = MinimaxWithAlphaBeta('O')
 
 results = {'X': 0, 'O': 0, 'draw': 0}
-for i in tqdm(range(10)):
+for i in tqdm(range(100)):
     ttt = TicTacToe([player_X, player_O])
     #ttt.set_debug(True)
     ttt.play()
     result = ttt.evaluate()
     results[result] += 1
+    #print('--------------------------')
 
 print(results)
 
-# ttt = TicTacToe([player_O, player_X], ['O', 'X', 'O', 'X', 'O', 'X', 'X', 7, 8])
+# ttt = TicTacToe([player_O, player_X], ['O', 1, 'X', 3, 4, 5, 6, 7, 'X'])
 # ttt.set_debug(True)
 # ttt.play()
 # print(ttt.evaluate())
