e4089d59ebb99730a98b4d7e19349a4668e6dae7
olga-gorun <olga@anyclip.com>
2019-10-17 16:13:13 +0300

    generic human_ai_game and num_agents_games_comparison finished

--- num_agents_games_comparison.py
+++ num_agents_games_comparison.py
@@ -2,8 +2,8 @@ from game import Game
 from games import *
 from agent import Agent
 from agents import *
-import argparse, inspect, re
-
+from tqdm import tqdm
+import argparse, inspect, re, time, gc
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -17,7 +17,7 @@ def str2bool(v):
 
 
 def add_parser_argument(parser, arg):
-    if arg.startswith('--c') or arg.startswith('--h') or arg.startswith('--g') and arg[3] in ['i', 'f', 'b', 's']:
+    if arg.startswith('--a1') or arg.startswith('--a2') or arg.startswith('--g') and arg[3] in ['i', 'f', 's', 'b']:
         argtype = None
         if arg[3] == 'i':
             argtype = int
@@ -33,17 +33,22 @@ def add_parser_argument(parser, arg):
         parser.add_argument(argname, type=argtype)
 
 
+def positive_int(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError("%s is an invalid positive int value" % value)
+    return ivalue
+
+
 def instantiate_agent_from_params(args, class_name, param_prefix, label):
     agent_cls = globals()[args[class_name]]
-    agent_params = {key[3:]: val for key, val in args.items() if re.match(f'^{param_prefix}(i|f|b|s_\.)', key)}
+    agent_params = {key[4:]: val for key, val in args.items() if re.match(f'^{param_prefix}(i|f|b|s_\.)', key)}
     agent = agent_cls(label, **agent_params)
     return agent
 
 
 avaliable_games = [cls for cls in globals() if cls != 'Game' and inspect.isclass(globals()[cls]) and issubclass(globals()[cls], Game)]
 avaliable_agents = [cls for cls in globals() if cls != 'Agent' and inspect.isclass(globals()[cls]) and issubclass(globals()[cls], Agent)]
-human_agents = [agent for agent in avaliable_agents if agent.startswith('Human')]
-ai_agents = [agent for agent in avaliable_agents if not agent.startswith('Human')]
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter,
@@ -52,8 +57,8 @@ parser = argparse.ArgumentParser(
 They will be passed to corresponding class at initialization.
 
 <class> allows the following values:
-c - computer agent class
-h - human agent class
+a1 - first agent class
+a2 - second agent class
 g - game class.
 
 <param_type> allows the following values:
@@ -62,17 +67,15 @@ i - integer
 b - boolean
 s - string
 
-Example: --ci.trials_num=100. Integer paramete trials_num=100 will be passed to constructor of class, specified by --computer-agent parameter
+Example: --a1i.trials_num=100. Integer paramete trials_num=100 will be passed to constructor of class, specified by --agent1 parameter
 
 Unknwon parameters that don't follow described pattern will be ignored.
 
 """)
 parser.add_argument("-g", "--game", help=f"game class", choices=avaliable_games, default="TicTacToe")
-parser.add_argument("-t", "--turn", type=int, help=f"human turn" , choices=[1,2], default="1")
-parser.add_argument("--computer-agent", help=f"computer agent class", choices=ai_agents, default="RandomAgent")
-parser.add_argument("--human-agent", help=f"human agent class", choices=human_agents, default="HumanTicTacToeCliAgent")
-parser.add_argument("-v", "--verbosity", help="increase output verbosity", action="store_true")
-
+parser.add_argument("-n", "--number", type=positive_int, help=f"games number", default=100)
+parser.add_argument("--agent1", help=f"first agent class", choices=avaliable_agents, default="RandomAgent")
+parser.add_argument("--agent2", help=f"second agent class", choices=avaliable_agents, default="RandomAgent")
 
 parsed, unknown = parser.parse_known_args()
 for arg in unknown:
@@ -84,19 +87,21 @@ print(args)
 game_cls = locals()[args['game']]
 game_params = {key[3:]: val for key, val in args.items() if re.match(f'^g(i|f|b|s_\.)', key)}
 
-human_agent = instantiate_agent_from_params(args, 'human_agent', 'h', game_cls.LABELS[args['turn']-1])
-ai_agent = instantiate_agent_from_params(args, 'computer_agent', 'c', game_cls.LABELS[1 - (args['turn']-1)])
+agents = [
+    instantiate_agent_from_params(args, 'agent1', 'h', game_cls.LABELS[0]),
+    instantiate_agent_from_params(args, 'agent2', 'c', game_cls.LABELS[1])
+]
 
-agents = [None, None]
-agents[args['turn']-1] = human_agent
-agents[1 - (args['turn']-1)] = ai_agent
+results = {game_cls.LABELS[0]: 0, game_cls.LABELS[1]: 0, 'draw': 0}
+for i in tqdm(range(args['number'])):
+    #start = time.time()
+    game = game_cls(agents, **game_params)
+    game.play()
+    result = game.evaluate()
+    results[result] += 1
+    del game
+    gc.collect()
+    #print(time.time()-start)
 
-game = game_cls(agents, **game_params)
-game.debug = True
-if args['verbosity']:
-    for agent in game.agents:
-        agent.debug = True
+print(results)
 
-print(f'You play {human_agent.label}')
-game.play()
-print(game.evaluate())
