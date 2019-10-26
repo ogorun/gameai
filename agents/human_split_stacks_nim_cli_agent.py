53fe8bd31ace96a8b3f176243492203f3ebe5364
Olga Gorun <olga@anyclip.com>
2019-10-26 14:45:31 +0300

    split stacks nim results added to evaluation CSV

--- agents/human_split_stacks_nim_cli_agent.py
+++ agents/human_split_stacks_nim_cli_agent.py
@@ -8,7 +8,6 @@ class HumanSplitStacksNimCliAgent(Agent):
     def move(self, game: SplitStacksNim, possible_states=None):
         available_moves = game.get_possible_moves()
         while True:
-            stacks_num = len(game.state)
             choice = input(f'Your move (stack number/first part size)? State: {game.state}')
             stack, part = [int(el.strip()) for el in choice.split('/')]
             for current_stack, current_part in available_moves:
--- play_test.py
+++ play_test.py
@@ -1,22 +1,26 @@
 from agents import *
 from agent_decorators.win_loose_decorator import WinLooseDecorator
 from games.tictactoe import TicTacToe
+from games.split_stacks_nim import SplitStacksNim
 from tqdm import tqdm
 import gc
 import time
 
 
 #player_X = RandomAgent('X')
-player_X = WinLooseDecorator(SingleTreeDepthSensitiveMCSTAgent('X'))
+player_X = WinLooseDecorator(SingleTreeDepthSensitiveMCSTAgent('1'))
 #player_X = MinimaxWithAlphaBeta('X')
 #player_X = SingleTreeDepthSensitiveMCSTAgent('X', 1.41, 10, 10)
 #player_O = DepthSensitiveMCSTAgent('O', 1.41, 10, 10)
-player_O = WinLooseDecorator(MinimaxWithAlphaBeta('O'))
+#player_O = WinLooseDecorator(MinimaxWithAlphaBeta('O'))
+#player_O = RandomAgent('2')
+player_O =  WinLooseDecorator(MinimaxWithAlphaBeta('2'))
 
-results = {'X': 0, 'O': 0, 'draw': 0}
+#results = {'X': 0, 'O': 0, 'draw': 0}
+results = {'1': 0, '2': 0, 'draw': 0}
 for i in tqdm(range(100)):
     #start = time.time()
-    ttt = TicTacToe()
+    ttt = SplitStacksNim(stack_size=10)
     #player_O.new_game()
     #player_X.new_game()
     #ttt.set_debug(True)
